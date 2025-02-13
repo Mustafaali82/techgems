7cf123df9a71994976b4a20a4e68a50202518833
Mustafa Ali <mustafaali8254@gmail.com>
2025-02-13 21:49:53 +0500

    Update Quotes.js

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -7,6 +7,13 @@ function Quotes() {
     "Success is not the key to happiness. Happiness is the key to success. - Albert Schweitzer",
     "If you can dream it, you can do it. - Walt Disney",
     "The most dangerous phrase in the language is, 'We've always done it this way. - Grace Hopper",
+    "Some people don’t like change, but you need to embrace change if the alternative is disaster. - Elon Musk",
+    "Stay hungry, stay foolish. - Steve Jobs",
+    "It’s easier to ask forgiveness than it is to get permission. - Grace Hopper",
+    "Try not to become a person of success, but rather try to become a person of value. - Albert Einstein",
+    "Talk is cheap. Show me the code - Linus Torvalds",
+    "I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it. -  Bill Gates",
+    "Bad programmers worry about the code. Good programmers worry about data structures and their relationships - Linus Torvalds"
   ];
 
   const [quotes] = useState(defaultQuotes);
