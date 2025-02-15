2773c7770680ffaa9bd9e4ea97e2ceca17118f1a
Mustafa Ali <mustafaali8254@gmail.com>
2025-02-15 14:38:24 +0500

    Update Quiz.js

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,45 +2,55 @@ import React, { useState, useEffect } from "react";
 
 const questions = [
   {
-    question: "Who created the C programming language?",
-    options: [
-      "Bjarne Stroustrup",
-      "Dennis Ritchie",
-      "James Gosling",
-      "Ken Thompson",
-    ],
+    question: "Who invented the C programming language?",
+    options: ["Bjarne Stroustrup", "Dennis Ritchie", "Guido van Rossum", "James Gosling"],
     answer: "Dennis Ritchie",
   },
   {
-    question: "Who is known as the father of the World Wide Web?",
-    options: ["Vint Cerf", "Tim Berners-Lee", "Robert Kahn", "Marc Andreessen"],
+    question: "What year was Python first released?",
+    options: ["1989", "1991", "1995", "2000"],
+    answer: "1991",
+  },
+  {
+    question: "Who is the co-founder of Google?",
+    options: ["Mark Zuckerberg", "Larry Page", "Bill Gates", "Steve Jobs"],
+    answer: "Larry Page",
+  },
+  {
+    question: "What company did Linus Torvalds create Linux while working at?",
+    options: ["IBM", "Microsoft", "University of Helsinki", "Red Hat"],
+    answer: "University of Helsinki",
+  },
+  {
+    question: "Which programming language did Grace Hopper help develop?",
+    options: ["COBOL", "Python", "Ruby", "Java"],
+    answer: "COBOL",
+  },
+  {
+    question: "Who is known as the inventor of the World Wide Web?",
+    options: ["Tim Berners-Lee", "Sergey Brin", "Alan Turing", "Richard Stallman"],
     answer: "Tim Berners-Lee",
   },
   {
-    question: "Who co-founded Google?",
-    options: ["Elon Musk", "Bill Gates", "Sergey Brin", "Mark Zuckerberg"],
-    answer: "Sergey Brin",
+    question: "What is Jordan Walke famous for creating?",
+    options: ["TensorFlow", "React.js", "Vue.js", "Angular"],
+    answer: "React.js",
   },
   {
-    question: "Who developed the Python programming language?",
-    options: [
-      "Guido van Rossum",
-      "Larry Page",
-      "Linus Torvalds",
-      "Grace Hopper",
-    ],
-    answer: "Guido van Rossum",
+    question: "Which programming language did Bjarne Stroustrup develop?",
+    options: ["Java", "C++", "C#", "Swift"],
+    answer: "C++",
   },
   {
-    question: "Who is the creator of Linux?",
-    options: [
-      "Richard Stallman",
-      "Linus Torvalds",
-      "Dennis Ritchie",
-      "Ken Thompson",
-    ],
-    answer: "Linus Torvalds",
+    question: "What was Alan Turing's major contribution to computing?",
+    options: ["Developed Unix", "Created the first compiler", "Conceptualized the Turing Machine", "Invented HTML"],
+    answer: "Conceptualized the Turing Machine",
   },
+  {
+    question: "Who co-founded Microsoft?",
+    options: ["Steve Jobs", "Bill Gates", "Elon Musk", "Mark Zuckerberg"],
+    answer: "Bill Gates",
+  }
 ];
 
 const Quiz = () => {
